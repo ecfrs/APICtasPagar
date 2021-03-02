2d90272520035a5fd196b1c5b00d1e6d603874b6
emerson.ferreira <ecfrs@bol.com.br>
2021-03-02 14:20:46 -0300

    new

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,28 @@
+﻿using CtasPagarAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtasPagarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : Controller
+    {
+        public IConfiguration _configuration;
+        private readonly AppDbContext _context;
+
+        public TokenController(IConfiguration config, AppDbContext context)
+        {
+            _configuration = config;
+            _context = context;
+        }               
+
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CtasPagarAPI.Models
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Conta> Contas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Conta>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.ValorOriginal).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.DataPagamento)
+                    .IsRequired();
+
+                entity.Property(e => e.DataVencimento)
+                    .IsRequired();
+
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
